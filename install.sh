1229eb3adcd06a77041273a723c28682d528a237
zhou zhenhua <citysir@qq.com>
2014-09-14 10:08:06 +0800

    修改 install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 yum install git hg -y
 
-mkdir -p /data/apps/gopath/src/code.google.com/p
-cd /data/apps/gopath/src/code.google.com/p
+export GOROOT=/usr/local/go
+export GOPATH=/data/apps/go
+
+mkdir -p /data/apps/go/src/code.google.com/p
+cd /data/apps/go/src/code.google.com/p
 
 hg clone https://code.google.com/p/goprotobuf/
 go install code.google.com/p/goprotobuf/proto
 ll /usr/bin/protoc
 
 go get git.apache.org/thrift.git/lib/go/thrift
-go get github.com/citysir/zpush
+
+go get github.com/citysir/zpush/join
+go get github.com/citysir/zpush/node
+go get github.com/citysir/zpush/offline
