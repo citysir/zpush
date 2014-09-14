b1292776be4874941624b7a4dacdf3c038cb7d3b
zhou zhenhua <citysir@qq.com>
2014-09-14 10:02:17 +0800

    增加 main 内容

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-yum install hg -y
+yum install git hg -y
 
 mkdir -p /data/apps/gopath/src/code.google.com/p
 cd /data/apps/gopath/src/code.google.com/p
@@ -8,3 +8,4 @@ go install code.google.com/p/goprotobuf/proto
 ll /usr/bin/protoc
 
 go get git.apache.org/thrift.git/lib/go/thrift
+go get github.com/citysir/zpush
